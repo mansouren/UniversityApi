2bcf76a30b1a47d39085f0ffc1a515ad26a674c1
M-Naeimabadi <Mansoure.naeimabadi@gmail.com>
2022-02-17 18:35:28 +0330

    Override Update in UserRepository for updating SecurityStamp

--- UniversityApi.Data/Repositories/UserRepository.cs
+++ UniversityApi.Data/Repositories/UserRepository.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using UniversityApi.Entities.Contracts;
 using UniversityApi.Entities.Models;
@@ -44,6 +45,35 @@ namespace UniversityApi.Data.Repositories
             return await TableAsNoTracking.AnyAsync(u => u.Username == username);
         }
 
+        public override Task UpdateAsync(User entity, CancellationToken cancellationToken, bool saveNow = true)
+        {
+            entity.SecurityStamp = Guid.NewGuid();
+            return base.UpdateAsync(entity, cancellationToken, saveNow);
+        }
+
+        public override void Update(User entity, bool saveNow = true)
+        {
+            entity.SecurityStamp = Guid.NewGuid();
+            base.Update(entity, saveNow);
+        }
+
+        public override Task UpdateRangeAsync(IEnumerable<User> entities, CancellationToken cancellationToken, bool saveNow = true)
+        {
+            foreach (var user in entities)
+            {
+                user.SecurityStamp = Guid.NewGuid();
+            }
+            return base.UpdateRangeAsync(entities, cancellationToken, saveNow);
+        }
+
+        public override void UpdateRange(IEnumerable<User> entities, bool saveNow = true)
+        {
+            foreach (var user in entities)
+            {
+                user.SecurityStamp = Guid.NewGuid();
+            }
+            base.UpdateRange(entities, saveNow);
+        }
 
     }
 }
